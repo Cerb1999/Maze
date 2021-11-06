5fcab5c2aa3bd852c98fa4f4cf0f7bf473098baa
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-11-06 17:46:13 +0100

    create a cell for ladders

---
+++ src/main/java/fr/ul/maze/model/LadderCell.java
@@ -0,0 +1,15 @@
+package fr.ul.maze.model;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class LadderCell extends Cell {
+    @Override
+    public boolean isWall() {
+        return false;
+    }
+
+    @Override
+    public Texture texture() {
+        return LADDER_TEXTURE.get();
+    }
+}
