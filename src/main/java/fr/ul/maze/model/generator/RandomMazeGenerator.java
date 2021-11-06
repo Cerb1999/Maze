644e43a3f234e1d4d9016b613a94e0466f5f9f85
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-11-06 16:56:32 +0100

    tweak random generation to terminate

--- src/main/java/fr/ul/maze/model/generator/RandomMazeGenerator.java
+++ src/main/java/fr/ul/maze/model/generator/RandomMazeGenerator.java
@@ -2,7 +2,9 @@ package fr.ul.maze.model.generator;
 
 import fr.ul.maze.model.*;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -10,29 +12,27 @@ public class RandomMazeGenerator extends AbstractMazeGenerator {
     /**
      * Randomly generates a maze by applying the randomized Prim's algorithm.
      *
-     * @return a procedurally generated maze.
+     * @return a procedurally generated random maze.
      * @see <a href="https://en.wikipedia.org/wiki/Maze_generation_algorithm#Randomized_Prim's_algorithm">Prim's algorithm</a>
      */
     @Override
     public Level generateMaze() {
-        // NOTE: these casts are required because of the type of <code>toArray</code>
-        //       they are NOT unsafe!
-        //       please don't remove them
-        Cell[][] grid = (Cell[][]) IntStream.range(0, Level.HEIGHT)
-                .mapToObj(_y -> (Cell[]) Stream.generate(WallCell::new)
+        Cell[][] grid = IntStream.range(0, Level.HEIGHT)
+                .mapToObj(_y -> Stream.generate(WallCell::new)
                         .limit(Level.WIDTH)
-                        .toArray())
-                .toArray();
+                        .toArray(Cell[]::new))
+                .toArray(Cell[][]::new);
 
         Random rnd = new Random();
 
+        // Start at a random position in the grid
         int startX = rnd.nextInt(Level.WIDTH);
         int startY = rnd.nextInt(Level.HEIGHT);
 
         // NOTE: it is necessary to use <code>LinkedList</code> because of <code>Collections.shuffle</code> taking
-        //       a <code>List<?></code> (else <code>ArrayDeque</code> is faster).
+        //       a <code>List<?></code> inside <code>this.pushNeighbours</code> (else <code>ArrayDeque</code> is faster).
         LinkedList<Position> walls = new LinkedList<>();
-        this.pushNeighbours(walls, new Position(startX, startY));
+        this.pushNeighbours(walls, rnd, new Position(startX, startY));
 
         while (!walls.isEmpty()) {
             Position wall = walls.pop();
@@ -40,26 +40,28 @@ public class RandomMazeGenerator extends AbstractMazeGenerator {
             if (this.isValidCell(grid, wall)) {
                 grid[wall.y()][wall.x()] = new PathCell();
 
-                this.pushNeighbours(walls, wall);
+                this.pushNeighbours(walls, rnd, wall);
             }
         }
 
-        return null;
+        // our grid should be the correct size :)
+        return new Level(grid);
     }
 
     /**
      * Inserts all the neighbours of the wall into the list of walls only if they are within the bounds of the grid.
      *
      * @param walls the list of wall to modify
-     * @param wall the wall to fetch neighbours of
+     * @param rnd a random generator
+     * @param wall  the wall to fetch neighbours of
      */
-    private void pushNeighbours(LinkedList<Position> walls, Position wall) {
+    private void pushNeighbours(LinkedList<Position> walls, Random rnd, Position wall) {
         for (Position pos : this.neighbours(wall)) {
             if (pos.x() >= 0 && pos.x() < Level.WIDTH && pos.y() >= 0 && pos.y() < Level.HEIGHT) {
                 walls.push(pos);
             }
         }
-        Collections.shuffle(walls, new Random());
+        Collections.shuffle(walls, rnd);
     }
 
     /**
@@ -83,7 +85,7 @@ public class RandomMazeGenerator extends AbstractMazeGenerator {
                 nb_adjacent_walls++;
         }
 
-        return nb_adjacent_walls >= 2;
+        return 4 - nb_adjacent_walls < 2;
     }
 
     /**
@@ -93,7 +95,7 @@ public class RandomMazeGenerator extends AbstractMazeGenerator {
      * @return an array containing all neighbouring positions
      */
     private Position[] neighbours(Position pos) {
-        return new Position[] {
+        return new Position[]{
                 new Position(pos.x(), pos.y() - 1), // NORTH
                 new Position(pos.x(), pos.y() + 1), // SOUTH
                 new Position(pos.x() - 1, pos.y()), // WEST
