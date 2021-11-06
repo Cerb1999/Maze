5ba447bdafe988ba39eb42eba164cdabbddbfacf
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-11-06 19:35:38 +0100

    document functions in the move controller

--- src/main/java/fr/ul/maze/controller/HeroMoveController.java
+++ src/main/java/fr/ul/maze/controller/HeroMoveController.java
@@ -5,6 +5,9 @@ import fr.ul.maze.model.Position;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * A basic controller to control the movements of the hero.
+ */
 public class HeroMoveController {
     private final AtomicReference<Level> modelReference;
 
@@ -12,6 +15,12 @@ public class HeroMoveController {
         this.modelReference = modelReference;
     }
 
+    /**
+     * Tries to move the hero in the given direction.
+     * If any non-practicable cell is encountered in this direction, no operations are performed.
+     *
+     * @param dir the direction to move in
+     */
     public void moveHero(final Direction dir) {
         this.modelReference.updateAndGet(level -> {
             Position heroPosition = level.getHeroPosition();
