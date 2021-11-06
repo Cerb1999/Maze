6d1a84361e0a2dcb3166743145ed51f1bc5ae49a
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-11-06 15:05:40 +0100

    improve some documentation

--- src/main/java/java/util/functional/Lazy.java
+++ src/main/java/java/util/functional/Lazy.java
@@ -7,31 +7,52 @@ import java.util.function.Supplier;
 /**
  * A lazy wrapper for a value of type <code>T</code>.
  *
- * @param <T>
- *
+ * @param <T> the type of values initialized by this lazy holder
  * @see <a href="https://stackoverflow.com/a/36280375/6718698">this StackOverflow post</a> for most of this interface.
  */
 @FunctionalInterface
 public interface Lazy<T> extends Supplier<T> {
+    /**
+     * Get the lazily generated value from this lazy holder.
+     * <p>
+     * Depending on the object generated, the first call to this method may be very slow.
+     *
+     * @return the object held in this lazy holder
+     */
+    @Override
+    default T get() {
+        return Cache.getInstance(this, this::init);
+    }
+
+    /**
+     * Initializes the lazy value.
+     * Because this interface is a functional interface, using a lambda of type <code>Lazy&lt;T&gt;</code> involves
+     * calling this function internally.
+     *
+     * @return a value lazily computed
+     */
+    T init();
+
     abstract class Cache<T> {
         /**
-         * {@implNote It is mandatory to use <code>Object</code> here instead of <code>T</code> as the value of the
-         * map, because <code>T</code> cannot be referenced from a static context.}
+         * @implNote
+         * It is mandatory to use <code>Object</code> here instead of <code>T</code> as the value of the
+         * map, because <code>T</code> cannot be referenced from a static context.
          */
-        private volatile static IdentityHashMap<Object, Object> values = new IdentityHashMap<>();
+        private static final IdentityHashMap<Object, Object> values = new IdentityHashMap<>();
 
         /**
          * Creates a new cached instance of a <code>T</code> only if none were already created from the
          * same lazy holder, else retrieves the one already created and returns it.
-         *
+         * <p>
          * This function is thread-safe. Multiple instances of the same lazy holder will not create multiple
          * times the same object in different threads.
          *
          * @param lazyHolder the lazy holder to create instances for
-         * @param creator the supplier used to create new instances of objects of type <code>T</code>
-         * @param <T> the type of objects to retrieve from the cache
+         * @param creator    the supplier used to create new instances of objects of type <code>T</code>
+         * @param <T>        the type of objects to retrieve from the cache
          * @return the cached value if one has already been created, else a new one generated (and cached) from
-         *         th supplier.
+         * th supplier.
          */
         public static synchronized <T> T getInstance(Lazy<T> lazyHolder, Supplier<T> creator) {
             Object instance = values.get(lazyHolder);
@@ -50,25 +71,4 @@ public interface Lazy<T> extends Supplier<T> {
             return (T) instance;
         }
     }
-
-    /**
-     * Get the lazily generated value from this lazy holder.
-     *
-     * Depending on the object generated, the first call to this method may be very slow.
-     *
-     * @return the object held in this lazy holder
-     */
-    @Override
-    default T get() {
-        return Cache.getInstance(this, this::init);
-    }
-
-    /**
-     * Initializes the lazy value.
-     * Because this interface is a functional interface, using a lambda of type <code>Lazy&lt;T&gt;</code> involves
-     * calling this function internally.
-     *
-     * @return a value lazily computed
-     */
-    T init();
 }
