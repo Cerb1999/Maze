9db092a5b072fa62bcb08e82fc466518ebdd2424
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-11-09 19:20:30 +0100

    add some dependencies for LibGDX apps to work with Nix

--- shell.nix
+++ shell.nix
@@ -5,11 +5,19 @@ pkgs.mkShell {
   name = "maze-nix-shell";
   version = "1.0.0";
 
+  nativeBuildInputs = with pkgs; [
+    # required to work with LibGDX
+    xorg.libXxf86vm
+    openal
+  ];
+
   buildInputs = with pkgs; [
     maven
     jdk11
     jetbrains.idea-community
   ];
+
+  LD_LIBRARY_PATH = "${pkgs.xorg.libXxf86vm}/lib:${pkgs.openal}/lib";
 
   shellHook = ''
     export JAVA_HOME=${pkgs.jdk11}/lib/openjdk
